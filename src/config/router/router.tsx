12b58778fa49c8c7e7f1e571cee73974c7694db8
20120454 <dat13102k2@gmail.com>
2023-12-19 17:52:05 +0700

    config react router

---
+++ src/config/router/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    element: <div>Home</div>,
+    path: "/",
+  },
+]);
+
+export default router;
