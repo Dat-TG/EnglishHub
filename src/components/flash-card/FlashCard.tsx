69137a0907498ca2d0512c120f9a5cca50a005b5
20120454 <dat13102k2@gmail.com>
2024-01-29 22:40:30 +0700

    refactor learn flashcard page

--- src/components/flash-card/FlashCard.tsx
+++ src/components/flash-card/FlashCard.tsx
@@ -4,41 +4,47 @@ import ReactCardFlip from "react-card-flip";
 import { IFlashcard } from "../../types/flashcard";
 
 interface Props {
-    card : IFlashcard;
+  card: IFlashcard;
 }
 
-const FlashCard: React.FC<Props> = ({card} : Props) => {
+const FlashCard: React.FC<Props> = ({ card }: Props) => {
+  const [isFlipped, setIsFlipped] = useState<boolean | undefined>(false);
 
-    const [isFlipped, setIsFlipped] = useState<boolean | undefined>(false);
+  const handleFlip = () => {
+    setIsFlipped((prevState) => !prevState);
+  };
 
-    const handleFlip = () => {
-        setIsFlipped((prevState) => !prevState);
-    };
-
-    return <>
-        <ReactCardFlip
-            isFlipped={isFlipped}
-            flipSpeedBackToFront={0.25}
-            flipSpeedFrontToBack={0.25}
-            flipDirection="vertical"
+  return (
+    <>
+      <ReactCardFlip
+        isFlipped={isFlipped}
+        flipSpeedBackToFront={0.25}
+        flipSpeedFrontToBack={0.25}
+        flipDirection="vertical"
+      >
+        <Card
+          sx={{
+            maxWidth: 345,
+            border: "1px solid #ccc",
+          }}
+          onClick={handleFlip}
         >
-            <Card sx={{ maxWidth: 345 }} onClick={handleFlip}>
-                <CardContent>
-                    <Typography variant="body2" >
-                        {card.front}
-                    </Typography>
-                </CardContent>
-            </Card>
+          <CardContent>
+            <Typography variant="body2">{card.front}</Typography>
+          </CardContent>
+        </Card>
 
-            <Card sx={{ maxWidth: 345 }} onClick={handleFlip}>
-                <CardContent>
-                    <Typography variant="body2" >
-                        {card.back}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </ReactCardFlip>
+        <Card
+          sx={{ maxWidth: 345, border: "1px solid #ccc" }}
+          onClick={handleFlip}
+        >
+          <CardContent>
+            <Typography variant="body2">{card.back}</Typography>
+          </CardContent>
+        </Card>
+      </ReactCardFlip>
     </>
-}
+  );
+};
 
 export default FlashCard;
--- src/features/flash-card/LearnFlashCardPage.tsx
+++ src/features/flash-card/LearnFlashCardPage.tsx
@@ -4,74 +4,90 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getFlashCardListById } from "../../config/api/flashcard/apiFlashcard";
 import { IconButton } from "@mui/material";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { useTranslation } from "react-i18next";
+import { Box } from "@mui/system";
 
 export default function LearnFlashCardPage() {
+  const { ListId } = useParams();
+  const [flashCardList, setFlashCardList] = useState<IFlashCardList>();
 
-    const { ListId } = useParams();
-    const [flashCardList, setFlashCardList] = useState<IFlashCardList>();
-
-    useEffect(() => {
-
-        if (ListId) {
-            getFlashCardListById(ListId).then(
-                (res) => {
-                    setFlashCardList(res);
-                }
-            )
-        }
-    }, [ListId]);
-
-    let cards = null;
-
-    if (flashCardList && flashCardList.flashcards) {
-        cards = flashCardList.flashcards.map((card) => (
-            <FlashCard key={card._id} card={card} />
-        ));
+  useEffect(() => {
+    if (ListId) {
+      getFlashCardListById(ListId).then((res) => {
+        setFlashCardList(res);
+      });
     }
+  }, [ListId]);
 
-    const [current, setCurrent] = useState(0);
+  let cards = null;
 
-    const previousCard = () => {
-        setCurrent(current - 1);
+  if (flashCardList && flashCardList.flashcards) {
+    cards = flashCardList.flashcards.map((card) => (
+      <FlashCard key={card._id} card={card} />
+    ));
+  }
+
+  const [current, setCurrent] = useState(0);
+
+  const previousCard = () => {
+    if (current > 0) {
+      setCurrent(current - 1);
+    } else {
+      setCurrent(cards.length - 1);
     }
+  };
 
-    const nextCard = () => {
-        setCurrent(current + 1);
+  const nextCard = () => {
+    if (current < cards.length - 1) {
+      setCurrent(current + 1);
+    } else {
+      setCurrent(0);
     }
-    return (
+  };
+  const { t } = useTranslation("global");
+  return (
+    <Box
+      p={{
+        xs: 1,
+        sm: 2,
+        md: 3,
+        lg: 4,
+        xl: 5,
+      }}
+    >
+      <div
+        style={{
+          marginBottom: "24px",
+        }}
+      >
+        <h1>{flashCardList?.name}</h1>
+      </div>
+      <div
+        className="cardNumber"
+        style={{
+          marginBottom: "24px",
+        }}
+      >
+        {t("Card")} {current + 1} / {cards?.length}
+      </div>
 
-        <div>
+      {cards && cards[current]}
 
-            <div className="cardNumber">
-                Card {current + 1} of {cards?.length}
-            </div>
+      <div
+        style={{
+          marginTop: "24px",
+        }}
+      >
+        <IconButton onClick={previousCard} size="large">
+          <ArrowBackIosIcon />
+        </IconButton>
 
-            {cards && cards[current]}
-
-            <div >
-                {current > 0 ? (
-                    <IconButton onClick={previousCard} size="large">
-                        <ArrowBackIosIcon />
-                    </IconButton>
-                ) : (
-                    <IconButton disabled size="large">
-                        <ArrowBackIosIcon />
-                    </IconButton>
-                )}
-
-                {cards && current < cards.length - 1 ? (
-                    <IconButton onClick={nextCard} size="large">
-                        <ArrowForwardIosIcon />
-                    </IconButton>
-                ) : (
-                    <IconButton disabled size="large">
-                        <ArrowForwardIosIcon />
-                    </IconButton>
-                )}
-            </div>
-        </div>
-
-    )
+        <IconButton onClick={nextCard} size="large">
+          <ArrowForwardIosIcon />
+        </IconButton>
+      </div>
+    </Box>
+  );
 }
